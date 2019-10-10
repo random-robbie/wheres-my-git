c6af09f59adf172262380b9c1156533e857ff0fb
Robbie <txt3rob@gmail.com>
2019-10-10 20:59:01 +0100

    Update where.sh

--- where.sh
+++ where.sh
@@ -2,9 +2,10 @@
 rm /tmp/new.tmp
 curl -sk $1 | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*/" | sort -u >> /tmp/new.tmp
 for ip in `cat /tmp/new.tmp`; do
-        if curl -sk $ip.git/config | grep -q "repositoryformatversion"; then
-        printf "$ip.git/config\n"
-        else
-        printf "Not Found\n"
+	if timeout 10s curl -sk $ip.git/config | grep -q "repositoryformatversion"; then
+    	printf "$ip.git/config\n"
+	echo $ip.git/config\n >> found.txt
+	else
+    	printf "Not Found\n"
 fi
 done
