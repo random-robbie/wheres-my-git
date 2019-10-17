6c7ea468652965988e63943f2b48dfb4dfb3e248
Robbie <txt3rob@gmail.com>
2019-10-17 17:17:00 +0100

    Update where.sh

--- where.sh
+++ where.sh
@@ -18,7 +18,7 @@ cat $1 | parallel -j 25 --bar curl -skL --connect-timeout 10 | grep -Eo "(http|h
 wait
 # Test for .git/config files and output to found.txt
 echo "Testing new targets..."
-cat /tmp/targets.tmp | parallel --citation -j 25 --bar check_target | tee found.txt
+cat /tmp/targets.tmp | parallel -j 25 --bar check_target | tee found.txt
 #rm /tmp/targets.tmp
 if [[ $(cat found.txt) == "" ]]; then
 	echo "No results found."
